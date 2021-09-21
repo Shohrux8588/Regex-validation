6e9e41cc4d7a4f3da3a2f372157bead8bbb50695
Shohrux8588 <shdavronbekovich@gmail.com>
2021-09-21 21:02:23 +0500

    regex

---
+++ validation.js
@@ -0,0 +1,26 @@
+// validation script here
+const inputs = document.querySelectorAll("input");
+
+const patters = {
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^([a-z\d\-\.]+)@([a-z\d\-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    password: /[\w@-]{8,20}/,
+    telephone: /^\d{11}$/,
+    slug: /^[a-z\d-]{8,20}$/
+}
+
+function check(e) {
+    const regex = patters[e.target.name];
+    const characters = e.target.value;
+    console.log(regex, characters);
+    if (regex.test(characters)) {
+        e.target.className = "valid";
+    } else {
+        e.target.className = "invalid";
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener("input", check);
+})
+
